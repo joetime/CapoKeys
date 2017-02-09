07a1c48c3ca6fffda60ec8c0b071f6803d0606ad
deepikaanantoju <ranantoj@cgifederal.com>
2017-02-09 02:07:07 -0500

    foldera and base js files added

---
+++ WebApplication1/scripts/capoapp.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+var capoApp = angular.module('capoApp', ['capoApp.controllers', 'capoApp.services', 'ngRoute']);
+
+angular.module('capoApp.controllers', []);
+angular.module('capoApp.services', []);
+
+capoApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view1', {
+        templateUrl: 'view1/view1.html',
+        controller: 'QueryCtrl'
+    });
+    $routeProvider.when('/view2', {
+        templateUrl: 'view2/view2.html',
+        controller: 'ResultCtrl'
+    });
+    $routeProvider.otherwise({ redirectTo: '/view1' });
+}]);
---
+++ WebApplication1/scripts/controllers/result.js
@@ -0,0 +1,5 @@
+﻿'use strict';
+angular.module('capoApp.controllers').controller('ResultCtrl', ['$scope', '$log', 'capoService', function ($scope, $log, mainService) {
+    $scope.name1 = mainService.name;
+    $log.info($scope.name1);
+}]);
